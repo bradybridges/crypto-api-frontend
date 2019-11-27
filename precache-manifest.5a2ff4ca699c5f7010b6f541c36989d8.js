a66eae1a291a92f12e6fcfe5ceed7788b6ec5bb2
Brady Bridges <bradyjbridges@gmail.com>
2019-11-27 12:14:14 -0700

    Updates

---
+++ precache-manifest.5a2ff4ca699c5f7010b6f541c36989d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa09eb1046e06f44030ab5a19a880252",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6c64e7ea0458aba09ab1",
+    "url": "./static/css/main.c9da437b.chunk.css"
+  },
+  {
+    "revision": "1f6fe294e95cfdf20406",
+    "url": "./static/js/2.278139f3.chunk.js"
+  },
+  {
+    "revision": "6c64e7ea0458aba09ab1",
+    "url": "./static/js/main.c4220770.chunk.js"
+  },
+  {
+    "revision": "5e2da83a535eaec3f5c0",
+    "url": "./static/js/runtime-main.9d84cdac.js"
+  }
+]);
